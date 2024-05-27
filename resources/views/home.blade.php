27dde21078a8b90dca536d759ca5e5d684e40f9c
agilalatas <agilalatas17@gmail.com>
2024-05-27 23:10:24 +0700

    homepage style

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<x-layout title="Beranda">
+    <section class="welcome my-4">
+        <div class="row gap-5 gap-lg-0">
+            <div class="col-lg-6 left-content">
+
+                <img src="{{ asset('assets/images/Lingkaran-non-Khima.png') }}" class="rounded mx-auto mx-lg-0 d-block"
+                    height="272">
+
+                <h1 class="text-center text-lg-start mt-5 mb-4">Early Life Nutrition Academy</h1>
+                <p class="text-center text-lg-start mb-5">Cegah Stunting, Ibu Sehat, Anak Cerdas, dan Keluarga Bahagia
+                </p>
+                <button type="button" class="btn btn-primary btn-lg mx-auto mx-lg-0 d-block">Gabung Sekarang</button>
+            </div>
+            <div class="col-lg-6 right-content">
+                <div class="d-flex gap-2 gap-lg-4 flex-column flex-xl-row">
+                    <div class="card py-4 px-5">
+                        <div class="card-body p-0">
+                            <h5 class="card-title text-body-tertiary">KONSULTASI GIZI</h5>
+                            <p class="card-text">Dengan ahli gizi yang berpengalaman.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card py-4 px-5">
+                        <div class="card-body p-0">
+                            <h5 class="card-title text-body-tertiary">ONLINE COURSE</h5>
+                            <p class="card-text">Untuk keluarga dan calon pengantin.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
