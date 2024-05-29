36dfd225ef124886eea091c53aa191d625af1390
agilalatas <agilalatas17@gmail.com>
2024-05-29 15:50:22 +0700

    fixing navbar active

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="Tentang Pokarez">
+    <h1>Halaman Tentang Pokarez</h1>
+</x-layout>
---
+++ resources/views/konsultasi.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="Konsultasi Pokarez">
+    <h1>Konsultasi Pokarez</h1>
+</x-layout>
